c316e5debd4beeb97ab1c78cbd3dee4491e6a90f
Samuel Vanmeenen Bachelard <svanmeen@z1r11p1.42lyon.fr>
2023-05-17 12:43:19 +0200

    Finished

--- fdf.h
+++ fdf.h
@@ -6,14 +6,14 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 13:54:13 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/05/17 12:26:57 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/05/17 12:40:35 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 # include "libft/libft.h"
-# include "minilibx/mlx.h"
+# include "minilibx-linux/mlx.h"
 # include "fcntl.h"
 # include "stdio.h"
 # include "math.h"
