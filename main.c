51cee3e41d950ae0c215f3147eab1a5c7354d36d
Samuel Vanmeenen Bachelard <svanmeen@z3r4p3.42lyon.fr>
2023-06-14 14:14:16 +0200

    Fixed Leak

--- main.c
+++ main.c
@@ -6,30 +6,11 @@
 /*   By: svanmeen <svanmeen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:42:58 by svanmeen          #+#    #+#             */
-/*   Updated: 2023/05/17 11:47:40 by svanmeen         ###   ########.fr       */
+/*   Updated: 2023/06/14 14:13:35 by svanmeen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-void	print_list(t_parse **list)
-{
-	t_parse	*tmp;
-	int		i;
-
-	tmp = *list;
-	if (!(*list))
-		ft_printf("Empty\n");
-	else
-	{
-		while (tmp)
-		{
-			ft_printf("x :%d y:%d z:%d\n", tmp->graph_x, tmp->graph_y, tmp->z);
-			tmp = tmp->next;
-			i++;
-		}
-	}
-}
 
 int	main(int argc, char **argv)
 {
@@ -38,10 +19,12 @@ int	main(int argc, char **argv)
 
 	points = malloc(sizeof(t_parse *));
 	if (input(argc, argv) || !points)
+	{
+		free(points);
 		return (EXIT_FAILURE);
+	}
 	*points = 0;
 	set = malloc(sizeof(t_param));
-	ft_memset(set, 0, sizeof(t_param *));
 	if (!set)
 		return (free_points(points));
 	if (ft_get_tab(argv[1], points, &(set->info)))
